ba0a860f2343f7d4e0f234ec8e4519ba40cc5204
nokep1978 <automehanick@gmail.com>
2022-06-20 21:45:36 +0700

    123

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+console.log(5+7);
+var second = 60;
+var minutes = 60;
+console.log('Секунд в часе  ' + second * minutes);
+var strings = "sSLslkj osdDFisj oisLLKdjfoij";
+var strings = strings.toLowerCase();
+var strings = strings .toUpperCase();
+console.log(strings);
